eccb3d1aebd7c25e815811cc6f0b3ab7f1805a42
yo-fenix <alejandro.iglesias@trifenix.io>
2021-02-17 10:19:02 -0300

    Estructura de versiones
    Estructura que será almacenada con las versiones y sus dependencias.

---
+++ trifenix.versions/model/CommitVersion.cs
@@ -0,0 +1,59 @@
+﻿namespace trifenix.versions.model
+{
+    /// <summary>
+    /// Detalles de un commit y su versión
+    /// </summary>
+    public class CommitVersion {
+
+        /// <summary>
+        /// Rama del commit
+        /// </summary>
+        public string branch { get; set; }
+
+        /// <summary>
+        /// Sección Major de la versión semántica
+        /// </summary>
+        public int Major { get; set; }
+
+        /// <summary>
+        /// Sección Minor de la versión semántica
+        /// </summary>
+        public int Minor { get; set; }
+
+        /// <summary>
+        /// Sección Patch de la versión semántica
+        /// </summary>
+        public int Patch { get; set; }
+
+        /// <summary>
+        /// Label de la versión
+        /// </summary>
+        public string PreReleaseLabel { get; set; }
+
+        /// <summary>
+        /// autonumérico que se incrementa, se usa en ramas que no son master.
+        /// </summary>
+        public int Preview { get; set; }
+
+        /// <summary>
+        /// Si una rama a modificar es dependant release, usará la versión del release.
+        /// </summary>
+        public bool DependantRelease { get; set; }
+
+        /// <summary>
+        /// Indica si una rama es feature.
+        /// </summary>
+        public bool IsFeature { get; set; }
+
+        /// <summary>
+        /// genera la versión en string.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{Major}.{Minor}.{Patch}.{PreReleaseLabel}.{Preview}";
+        }
+    }
+
+
+}
---
+++ trifenix.versions/model/VersionStructure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trifenix.versions.model
+{
+
+    /// <summary>
+    /// Estructura de un paquete y sus versiones, a ser almacenada
+    /// en en repositorio
+    /// </summary>
+    public class VersionStructure
+    {
+        /// <summary>
+        /// Nombre del paquete.
+        /// </summary>
+        public string PackageName { get; set; }
+
+
+        /// <summary>
+        /// Tipo del paquete (json / nuget).
+        /// </summary>
+        public PackageType PackageType { get; set; }
+
+        /// <summary>
+        /// repositorio de github del paquete en https.
+        /// </summary>
+        public string GithubHttp { get; set; }
+
+        /// <summary>
+        /// Repositori del github del paquete en ssh.
+        /// </summary>
+        public string GithubSsh { get; set; }
+
+
+        /// <summary>
+        /// todas las versiones que se han generado.
+        /// </summary>
+        public List<CommitVersion> Versions { get; set; } = new List<CommitVersion>();
+
+
+        /// <summary>
+        /// listado con los paquetes dependientes del paquete principal.
+        /// </summary>
+        public List<Dependency> Dependencies { get; set; }
+
+
+    }
+
+
+}
