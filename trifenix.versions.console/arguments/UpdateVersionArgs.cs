a5e8f41cad1912b881caff266537b4a74a55cc23
yo-fenix <alejandro.iglesias@trifenix.io>
2021-02-16 21:19:41 -0300

    comandos operando

--- trifenix.versions.console/arguments/UpdateVersionArgs.cs
+++ trifenix.versions.console/arguments/UpdateVersionArgs.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using trifenix.versions.model;
 
 namespace trifenix.arguments
 {
@@ -13,6 +14,9 @@ namespace trifenix.arguments
     [Verb("update", HelpText = "Actualiza el modelo de datos en github, con la nueva versión a generar y la retorna")]
     public class UpdateVersionArgs
     {
+        /// <summary>
+        /// github donde se almacenará la estructura de versiones.
+        /// </summary>
         [Value(0, Required = true, HelpText = "Url o ssh de la url de git del proyecto, la gestión de versiones se hará en este repositorio")]
         public string GitAddress { get; set; }
 
@@ -37,12 +41,45 @@ namespace trifenix.arguments
         public string branch { get; set; }
 
         /// <summary>
-        /// 
+        /// Token del github.
         /// </summary>
         [Option('t', "token", Required = true, HelpText = "token de github")]
         public string Token { get; set; }
 
 
+        /// <summary>
+        /// build de azure devops.
+        /// </summary>
+        [Option('b', "build", Required = false, HelpText = "Build, no requerido para la rama master.")]
+        public string packageName { get; set; }
 
+
+        /// <summary>
+        /// nombre del paquete a actualizar.
+        /// </summary>
+        [Option('n', "name", Required = true, HelpText = "nombre del paquete")]
+        public string Build { get; set; }
+
+        /// <summary>
+        /// Tipo del paquete
+        /// </summary>
+        [Option('k', "ktype", Required = true, HelpText = "tipo del paquete")]
+        public PackageType packageType { get; set; }
+
+        /// <summary>
+        /// si la rama depende del release, se usará release para la nueva versión, sino la master.
+        /// </summary>
+        [Option('d', "dependant", Required = false, Default = false, HelpText = "si la actualización depende del release, se usará release para la nueva versión, sino la master")]
+        public bool DependantRelease { get; set; }
+
+
+        /// <summary>
+        /// muestra de valores de la clase
+        /// </summary>
+        /// <returns>valores de la clase, solo por debug.</returns>
+        public override string ToString()
+        {
+            return $@"gitaddress = {GitAddress} \n username = {username} \n email = {email} \n branch = {branch} \n token = {Token} \n package-name={packageName} \n package-type:{packageType} \n build:{Build} \n dependant = {DependantRelease}";
+        }
     }
 }
