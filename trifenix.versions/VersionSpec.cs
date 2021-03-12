33c4ea05617c46b0a31ab857410884a3117b128d
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-12 00:47:05 -0300

    Se elimina mensaje en donde deberia ir solo la versión
    el propósito del programa es entregar la versión

--- trifenix.versions/VersionSpec.cs
+++ trifenix.versions/VersionSpec.cs
@@ -473,7 +473,7 @@ namespace trifenix.versions
 
             
             var refactorVersions = version.Versions.Where(s => s.LastUpdate != null && s.Branch.Equals(branch)).OrderByDescending(s => s.LastUpdate).Take(5).ToList();
-            message($"Se encontraron {version.Versions.Count} versiones, se tomarán solo {refactorVersions.Count}");
+            
             version.Versions = refactorVersions;
 
             var fileFullpath = utils.GetPackageFullPath(string.Empty, packageName, packageType);
